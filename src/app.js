9070716767dae039caae487964dc070eecaa9d56
heart980 <86405054+heart980@users.noreply.github.com>
2023-04-26 19:21:19 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export default {
     /*网站设置*/
     title:'AI - 快速导航',  //网站标题
     logoText:'LK',    //网站文字logo,不填则不显示
-    navStyle:2,      //网址导航样式：1经典样式，2卡片样式
+    navStyle:1,      //网址导航样式：1经典样式，2卡片样式
     connect:{       //右上角反馈按钮
         text:'反馈',
         href:'https://vika.cn/share/shr0ydKWGscvRnUCEfQqN'
@@ -10,7 +10,7 @@ export default {
 
     /*以下为开启或关闭组件，0代表关闭，1代表开启*/
     showBanner:1,   //轮播banner
-    showAddWeb:1,   //自定义添加网址
+    showAddWeb:0,   //自定义添加网址
     showTip:1,      //鼠标悬浮提示
     showLogo:1,     //顶部的网站logo图片
 }
